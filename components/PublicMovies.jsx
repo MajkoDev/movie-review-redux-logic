39c81748cee56331477c924591feb55a84ef44c0
Marian <hellomariandev@gmail.com>
2023-06-10 19:05:46 +0200

    read data and display in UI

--- components/PublicMovies.jsx
+++ components/PublicMovies.jsx
@@ -1,10 +1,18 @@
 import React from "react";
+import { useSelector } from "react-redux";
 
 const PublicMovies = () => {
+  const publicMovies = useSelector((store) => store.public);
+
+  const renderMovies = publicMovies.map((movie) => (
+    <h3 key={movie.id}>{movie.name}</h3>
+  ));
+
   return (
     <div className="container" id="public-movies">
       <h2>Public Movies</h2>
       <p>here you can choose what movies you have watched or wish to see</p>
+      {renderMovies}
     </div>
   );
 };
