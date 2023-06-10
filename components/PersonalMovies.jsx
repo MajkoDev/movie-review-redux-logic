620753804fee7452412bc6d75de0396c4031b2e0
Marian <hellomariandev@gmail.com>
2023-06-10 18:51:41 +0200

    main components

---
+++ components/PersonalMovies.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const PersonalMovies = () => {
+  return (
+    <div className="container" id="personal-movies">
+      <h2>Personal Movies</h2>
+      <p>this is profile, where you can see your movies</p>
+    </div>
+  );
+};
+
+export default PersonalMovies;
---
+++ components/PublicMovies.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const PublicMovies = () => {
+  return (
+    <div className="container" id="public-movies">
+      <h2>Public Movies</h2>
+      <p>here you can choose what movies you have watched or wish to see</p>
+    </div>
+  );
+};
+
+export default PublicMovies;
