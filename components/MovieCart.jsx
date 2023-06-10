b29584fd167674d1472c6f698c7c0712126830ea
Marian <hellomariandev@gmail.com>
2023-06-10 18:51:55 +0200

    movie component

---
+++ components/MovieCart.jsx
@@ -0,0 +1,12 @@
+export default function MovieCart() {
+  return (
+    <div className="movie-cart">
+      <h2>movie title</h2>
+      <p className="small-text">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem sint odit
+        reiciendis beatae sunt, placeat assumenda perferendis, expedita
+        cupiditate voluptatibus nobis! In, asperiores!
+      </p>
+    </div>
+  );
+}
