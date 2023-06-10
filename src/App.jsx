40190f2f1f1d28ed1428378923cb0a637ecfc067
Marian <hellomariandev@gmail.com>
2023-06-10 18:51:44 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
+import PersonalMovies from "../components/PersonalMovies";
+import PublicMovies from "../components/PublicMovies";
+
 function App() {
   return (
     <div className="App">
-      <h1>This is App!</h1>
-      <h2>heading 2</h2>
-      <h3>heading 3</h3>
-      <p>paragraph</p>
+      
+      <PublicMovies />
+      <PersonalMovies />
+
+
+      
     </div>
   );
 }
