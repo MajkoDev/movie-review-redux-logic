fe47582d9b14f4f05f1096a8afd3529292f14585
Marian <hellomariandev@gmail.com>
2023-06-10 18:34:08 +0200

    app set up

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div className="App">
+      <h1>This is App!</h1>
+      <h2>heading 2</h2>
+      <h3>heading 3</h3>
+      <p>paragraph</p>
+    </div>
+  );
+}
+
+export default App;
