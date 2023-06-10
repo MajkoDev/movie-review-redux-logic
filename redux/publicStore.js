ff11862345108e69d04e081a13e2401ac3cb9380
Marian <hellomariandev@gmail.com>
2023-06-10 19:05:34 +0200

    add data

--- redux/publicStore.js
+++ redux/publicStore.js
@@ -1,6 +1,10 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-const initialState = [];
+const initialState = [
+  {id: 1, name: 'Citizen Kane'},
+  {id: 2, name: 'Inception'},
+  {id: 3, name: 'Drive My Car'}
+];
 
 export const publicStore = createSlice({
   name: "public",
